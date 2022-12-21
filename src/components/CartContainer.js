0aa6c1bc919f4cbb8528e1a82a50966b0d7ce55b
forrestmorrison <forrestmorrison@gmail.com>
2022-12-21 15:29:22 -0600

    added useDispatch & clearCart reducer

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,21 +1,24 @@
 import React from 'react'
 import CartItem from './CartItem';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearCart } from '../features/cart/cartSlice';
 
 const CartContainer = () => {
 
     const { cartItems, total, amount } = useSelector((store) => store.cart);
+    const dispatch = useDispatch();
 
-    if (amount < 1) {
-        return (
-            <section className='cart'>
-                <header>
-                    <h2>your bag</h2>
-                    <h4 className='empty-cart'>is currently empty</h4>
-                </header>
-            </section>
-        );
-    }
+    // if (amount < 1) {
+    //     return (
+    //         <section className='cart'>
+    //             <header>
+    //                 <h2>your bag</h2>
+    //                 <h4 className='empty-cart'>is currently empty</h4>
+    //             </header>
+    //         </section>
+    //     );
+    // }
+
     return (
         <section className='cart'>
             <header>
@@ -33,10 +36,10 @@ const CartContainer = () => {
                         total <span>${total}</span>
                     </h4>
                 </div>
-                <button className='btn clear-btn'>clear cart</button>
+                <button className='btn clear-btn' onClick={() => dispatch(clearCart())}>clear cart</button>
             </footer>
         </section>
     );
 }
 
-export default CartContainer
+export default CartContainer;
